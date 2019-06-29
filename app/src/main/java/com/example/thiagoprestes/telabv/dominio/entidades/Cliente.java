776258dc6dd12ae75a80479d7b867e14e6256345
FABRICAgci <astonfla87@gmail.com>
2019-06-28 21:16:47 -0400

    first commit

---
+++ app/src/main/java/com/example/thiagoprestes/telabv/dominio/entidades/Cliente.java
@@ -0,0 +1,18 @@
+package com.thiagoprestes.telabv.dominio.entidades;
+
+import java.io.Serializable;
+
+
+public class Cliente implements Serializable{
+
+    public int codigo;
+    public String nome;
+    public String endereco;
+    public String email;
+    public String telefone;
+
+    public Cliente(){
+        codigo = 0;
+    }
+
+}
